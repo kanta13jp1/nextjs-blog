3ac0fac2648a70b3dbccda1e033f2aa24001234d
kanta13jp1 <kanta13jp@gmail.com>
2022-05-23 08:54:43 +0900

    fetch api

--- pages/blog.js
+++ pages/blog.js
@@ -12,8 +12,10 @@ function Blog({ posts }) {
 // This function gets called at build time
 export async function getStaticProps() {
     // Call an external API endpoint to get posts
-    const res = await fetch('https://hello-world-1-6ccgk2l43a-an.a.run.app/assets/data.json')
+    const res = await fetch('https://hello-world-1-6ccgk2l43a-an.a.run.app/encode')
     const posts = await res.json()
+
+    console.log(posts)
   
     // By returning { props: { posts } }, the Blog component
     // will receive `posts` as a prop at build time
