cdeb1e66f1f69fd07f93deb00ee83fe2c6f47591
kanta13jp1 <kanta13jp@gmail.com>
2022-06-09 06:21:20 +0900

    308

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,14 @@
 function About() {
     return <div>About Test</div>
 }
-  
+
+export async function getStaticProps(context) {
+    return {
+        redirect: {
+            destination: '/',
+            permanent: true, // triggers 308
+        },
+    };
+}
+
 export default About
